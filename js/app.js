da80fc5c9a715754a1599acbd9474cf6cb9ba9ea
alyssalbailey <alyssabailey@me.com>
2021-06-26 01:38:27 -0400

    Adding the data badges to the page with innerHTML

--- js/app.js
+++ js/app.js
@@ -3,40 +3,42 @@ const urlAPI = `https://teamtreehouse.com/alyssalbailey.json`;
 const achievementContainer = document.querySelector(".achievement-container");
 
 
-// Fetch data from the API - from the employee directory project - not finished for this project
+// Fetch data from the API 
 fetch(urlAPI)
     .then(res => res.json())
-    .then(res => console.log(res))
+    .then(res => console.log(res.badges))
     .catch(err => console.log(err))
 
 function displayAchievements(achievementBadge) {
-    employees = employeeData;
-    let badgeHTML = ""; // store the employee HTML as we create it
+    achievements = achievementBadge;
+    let badgeHTML = ""; // store the achievement badges HTML
 
-    /*
-        function displayEmployees(employeeData){
-            employees = employeeData;
-            let employeeHTML = ""; // store the employee HTML as we create it */
+    // loop through each achievement badge and create HTML markup
+    achievements.forEach((achievement, index) => {
+        let name = achievement.name;
+        let date = achievement.earned_date;
+        let iconUrl = achievement.icon_url;
+        let id = achievement.id;
+        let classUrl = achievement.url;
+               
+        
 
-    /* loop through each employee and create HTML markup
-    employees.forEach((employee, index) => {
-        let name = employee.name;
-        let email = employee.email;
-        let city = employee.location.city;
-        let picture = employee.picture; */
-
-    employeeHTML += `
-            <div class="card" data-index="${index}">
-                <img class="avatar" src="${picture.large}"/>
-                <div class="text-container">
-                    <h2 class="name">${name.first} ${name.last}</h2>
-                    <p class="email">${email}</p>
-                    <p class="address">${city}</p>
+    badgeHTML += `
+            <div class="card mb-3 style="max-width: 500px;" data-index="${index}">
+                <div class="row g-0">
+                    <div class"col-md-4 order-1 bg-light">
+                        <img src="${iconUrl}"class="img-fluid rounded-start achievement-icon" alt="Achievement icon"/>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">${name}</h5>
+                            <p class="card-text">${classUrl}</p>
+                            <p class="card-text"><small class="text-muted">${date}</small></p>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            </div>    
         `
-};
+});
 achievementContainer.innerHTML = badgeHTML;
-;
-
-
+};
