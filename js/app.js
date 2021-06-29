c3b0269ce773263b0562cb15e5959d36787cde4e
alyssalbailey <alyssabailey@me.com>
2021-06-29 01:13:51 -0400

    modifying the achievement badge horizontal cards to fit into the container.

--- js/app.js
+++ js/app.js
@@ -24,28 +24,27 @@ function displayBadges(achievementBadge) {
         let iconUrl = badge.icon_url;
         let date = badge.earned_date;
         
-    //badgeHTML += `
-    <div class="row mb-2">
-    <div class="col-md-6">
-        <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-            <div class="col p-4 d-flex flex-column position-static">
-                <strong class="d-inline-block mb-2 text-primary">HTML CSS JAVASCRIPT</strong>
-                <h3 class="mb-3">${badge.name}</h3>
-                <div class="checkmark-text">
-                    <img src="images/checkmark.svg" class="checkmark" alt="checkmark" height="28" width="28">
-                    <div class="d-flex flex-column">
-                        <p class="text-muted fw-bold m-0">Achieved</p>
-                        <p class="mb-1 text-muted">${badge.earned_date}</p> 
+    badgeHTML += `
+        <div class="row mb-2">
+            <div class="col-md-4">
+                <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                    <div class="col p-4 d-flex flex-column position-static">
+                        <h4 class="mb-3">${badge.name}</h4>
+                        <div class="checkmark-text">
+                            <img src="images/checkmark.svg" class="checkmark" alt="checkmark" height="28" width="28">
+                            <div class="d-flex flex-column">
+                                <p class="text-muted fw-bold m-0">Achieved</p>
+                                <p class="mb-1 text-muted">${badge.earned_date}</p> 
+                            </div>
+                        </div>  
                     </div>
-                </div>  
-            </div>
-            <div class="col-auto d-none d-lg-block bg-light p-4">
-            <img width="150" height="170" src="${badge.icon_url}" alt="Treehouse Badge Icon">
-            </div>
+                    <div class="g-0 col-auto d-none d-lg-block bg-light p-4">
+                    <img width="90" height="110" src="${badge.icon_url}" alt="Treehouse Badge Icon">
+                    </div>
+                </div>
+            </div> 
         </div>
-    </div>
-
-
+    </div>    
 
         `
 });
